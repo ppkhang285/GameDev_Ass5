e8a86081871e7af9aa26625a17c7616092d63250
Phạm Phú Khang <ppkhang285@gmail.com>
2024-12-05 17:12:57 +0700

    Update Anim for Knight
    - Attack
    - Hit
    - Dodge
    - Idle
    - Dead

--- Ass5/Assets/Scripts/Characters/Character.cs
+++ Ass5/Assets/Scripts/Characters/Character.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Character : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
     private CharacterStats characterStats;
+
+    private Animator animator;
 
     public CharacterStats stats
     {
@@ -18,13 +21,66 @@ public class Character : MonoBehaviour
 
     void Start()
     {
+        animator = GetComponent<Animator>();
+
         CurrentHP = characterStats.hp;
         CurrentDamage = characterStats.damage;
     }
 
+
+
     // Update is called once per frame
     void Update()
     {
-        
+        TestAnim();
     }
+
+
+    public void TestAnim()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("isRunning", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("isRunning", false);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            animator.SetTrigger("attack");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("hit");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetFloat("x", -1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetFloat("x", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetFloat("y", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetFloat("y", -1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("dodge");
+        }
+
+    }
+   
+
 }
