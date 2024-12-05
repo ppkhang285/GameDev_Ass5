929edd1bed6df6cdf088e4c329157de972e97ef7
sitrongtrang <sitrongtrang@gmail.com>
2024-12-05 14:56:10 +0700

    add gameplay manager

---
+++ Ass5/Assets/Scripts/Gameplay/GameplayManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public static GameplayManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
